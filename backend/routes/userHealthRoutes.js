bbe064851dfb206a5800b336a015956de67dd602
Mansi Negi <mansinegi0786@gmail.com>
2025-11-26 13:25:07 +0530

    bug fix in login page

--- backend/routes/userHealthRoutes.js
+++ backend/routes/userHealthRoutes.js
@@ -1,15 +1,9 @@
-// const express = require('express')
-// const router = express.Router();
-// const {sethealthProfile,gethealthProfile} = require("../controller/userHealthRoutes") 
-// router.post('/sethealthProfile',sethealthProfile);
-// router.get('/gethealthProfile/:userId',gethealthProfile);
-// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const { sethealthProfile, gethealthProfile } = require("../controller/userHealthRoutes"); 
 const { restrictUser } = require("../middlewares/auth");
 
-// ✅ Auth-protected routes
 router.post("/sethealthProfile", restrictUser, sethealthProfile);
 router.get("/gethealthProfile", restrictUser, gethealthProfile);
 
